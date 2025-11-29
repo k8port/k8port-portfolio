3f18c04c6bfc1361de24ff23593b511088611051
k8port <k8portalatin@gmail.com>
2025-11-28 23:24:54 -0600

    prettier eslint fixes

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss';
+import { themeConfig } from './app/styles/theme/theme.config';
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */ 
+export default {
+    content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
+    theme: {
+        extend: {
+            colors: themeConfig.colors,
+            fontFamily: themeConfig.typography,
+            boxShadow: themeConfig.shadows,
+            spacing: themeConfig.spacing,
+            screens: themeConfig.screens,
+            textShadow: themeConfig.textShadow,
+            transitionProperty: themeConfig.transition,
+            zIndex: themeConfig.zIndex,
+        },
+    },
+    plugins: [],
+} satisfies Config;
