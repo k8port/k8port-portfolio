dc63b8262a9d25b44acec1c492d6a5a277d18668
k8port <k8portalatin@gmail.com>
2026-03-30 04:44:06 -0500

    13-create-blog

--- app/ui/landing/vector/AnimatedMuseGroup.tsx
+++ app/ui/landing/vector/AnimatedMuseGroup.tsx
@@ -50,6 +50,125 @@ export const AnimatedMuseGroup = ({ className }: AnimatedMuseGroupProps) => {
                 z-999
             `}
             />
+            <AnimatedMuseSVG
+                className={`
+                absolute inset-x-0 top-50 left-50 h-10
+                opacity-85
+                z-999
+            `}
+            />
+            <AnimatedMuseSVG
+                className={`
+                absolute inset-x-0 top-60 left-40 h-10
+                opacity-85
+                z-999
+            `}
+            />
+            <AnimatedMuseSVG
+                className={`
+                absolute inset-x-0 top-80 left-50 h-10
+                opacity-85
+                z-999
+            `}
+            />
+            <AnimatedMuseSVG
+                className={`
+                absolute inset-x-0 top-70 left-40 h-10
+                opacity-85
+                z-999
+            `}
+            />
+            <AnimatedMuseSVG
+                className={`
+                absolute inset-x-0 top-60 left-60 h-10
+                opacity-85
+                z-999
+            `}
+            />
+            <AnimatedMuseSVG
+                className={`
+                absolute inset-x-0 top-50 left-50 h-10
+                opacity-85
+                z-999
+            `}
+            />
+            <AnimatedMuseSVG
+                className={`
+                absolute inset-x-0 top-90 left-40 h-10
+                opacity-85
+                z-999
+            `}
+            />
+            <AnimatedMuseSVG
+                className={`
+                absolute inset-x-0 top-100 left-50 h-10
+                opacity-85
+                z-999
+            `}
+            />
+            <AnimatedMuseSVG
+                className={`
+                absolute inset-x-0 top-110 left-40 h-10
+                opacity-85
+                z-999
+            `}
+            />
+            <AnimatedMuseSVG
+                className={`
+                absolute inset-x-0 top-120 left-60 h-10
+                opacity-85
+                z-999
+            `}
+            />
+            <AnimatedMuseSVG
+                className={`
+                absolute inset-x-0 top-130 left-50 h-10
+                opacity-85
+                z-999
+            `}
+            />
+            <AnimatedMuseSVG
+                className={`
+                absolute inset-x-0 top-140 left-50 h-10
+                opacity-85
+                z-999
+            `}
+            />
+            <AnimatedMuseSVG
+                className={`
+                absolute inset-x-0 top-150 left-40 h-10
+                opacity-85
+                z-999
+            `}
+            />
+            <AnimatedMuseSVG
+                className={`
+                absolute inset-x-0 top-160 left-50 h-10
+                opacity-85
+                z-999
+            `}
+            />
+            <AnimatedMuseSVG
+                className={`
+                absolute inset-x-0 top-170 left-40 h-10
+                opacity-85
+                z-999
+            `}
+            />
+            <AnimatedMuseSVG
+                className={`
+                absolute inset-x-0 top-180 left-60 h-10
+                opacity-85
+                z-999
+            `}
+            />
+            <AnimatedMuseSVG
+                className={`
+                absolute inset-x-0 top-200 left-50 h-10
+                opacity-85
+                z-999
+            `}
+            />
         </div>
     );
 };
